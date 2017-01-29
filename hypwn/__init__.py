4d65f7bf4b155c117c11f0781c45337c28283363
Jacob Errington <git@mail.jerrington.me>
2017-01-29 10:28:16 -0500

    static files again

--- hypwn/__init__.py
+++ hypwn/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__, static_url_path='/frontend/dist')
+app = Flask(__name__, static_url_path='/static', static_folder='frontend/dist')
 
 from . import bus
 
