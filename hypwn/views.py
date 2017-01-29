1c7c01841f13610c3b556f00af832c903b45bf0a
Jacob Errington <git@mail.jerrington.me>
2017-01-29 10:23:29 -0500

    static routes

--- hypwn/views.py
+++ hypwn/views.py
@@ -1,6 +1,10 @@
 from . import app, HYPE_BUS
-from flask import redirect
+from flask import redirect, send_from_directory
 
 @app.route('/')
 def index():
     return redirect('/static/index.html')
+
+@app.route('/static/<p>')
+def static(p):
+    return send_from_directory(p)
